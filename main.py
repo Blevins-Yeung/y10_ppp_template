ffcdcfab0bb7774376b5f1cc0b34876cc57b457f
Blevins-Yeung <byeung@connect.kellettschool.com>
2025-04-01 02:52:17 +0000

    test

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 # testing
 
+def startup():
+    global player_1
+    global player_2
+
+    player_1 = input("Player 1: Enter your name ")
+    player_2 = input("Player 2: Enter your name ")
+    
+
+
+def make_grid():
+    pass
+
+def player_turn():
+    pass
+
+def input_column():
+    column = input(f"{player_1} which column would you like to drop your tile in (A-G)? ")
+
+
+def check_column():
+    pass
+
+def drop_tile():
+    
+

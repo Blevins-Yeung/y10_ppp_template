ed2c0410209dc9d85fb63e0ffedb350a0c034ee3
Blevins-Yeung <byeung@connect.kellettschool.com>
2025-06-03 03:51:04 +0000

    no message

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # testing
-valid_columns = ["a", "b", "c", "d", "e", "f", "g"]
+valid_columns = ["A", "B", "C", "D", "E", "F", "G"]
 def startup():
     global player_1
     global player_2
@@ -17,28 +17,35 @@ def make_grid():
 
 
 def input_column(current_player):
-    column = input(f"{current_player} which column would you like to drop your tile in (A-G)? ")
+    column = input(f"{current_player} which column would you like to drop your tile in (A-G)? ").upper()
     return column
 
-def check_column(column, row):
+def check_column(column, grid):
     if column in valid_columns:
-        if row == 1:
-            return False
-        else:
+        if grid[0][ord(column) - 65] == " ":
             return True
+        else:
+            return False
     else:
         return False
 
-def drop_tile(column, row):
-    pass
+def drop_tile(column, grid):
+    for i in range(6):
+        pass
 
 def update_grid(grid):
     pass
 
 def print_grid(grid):
+    
+    print(" A B C D E F G ")
     for row in grid:
+        print("|", end = "")
         for square in row:
-            print(square, end = " ")
+            print(square +"|", end = "")#, end = " "
+        print()
+    print("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
+        
 
 def check_horizontal():
     pass
@@ -76,12 +83,19 @@ def player_turn(current_player):
 
 def main():
     startup()
-    grid = make_grid()
+    grid = make_grid() 
     print_grid(grid)
     end_game = False
     current_player = None
     while end_game == False:
         current_player = get_next_player(current_player)
+        column = input_column(current_player)
+        column_validity = check_column(column, grid)
+        if column_validity == True:
+            drop_tile()
+
+
+
 
 
 
